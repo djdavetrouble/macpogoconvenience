05a7de596c041a81fd64a00038097eeb2a89e94d
djdavetrouble <dave.trouble@gmail.com>
2018-08-10 11:25:21 -0400

    Snorlax
    zzzz

--- PoGo-Convenience-chopped and screwed/ivsmackdown.sh
+++ PoGo-Convenience-chopped and screwed/ivsmackdown.sh
@@ -3,14 +3,14 @@ for p in perfects/*; do
 name=${p##*/}
 value=$(cat $p)
 shortcuts create "$name" "$value"
-echo "$name" "$value"
+echo "Adding shortcut for Perfect $name with value $value"
 sleep .2
 done
 
 for t in trashes/*; do
 name=${t##*/}
 value=$(cat $t)
-echo "$t" "$value"
+echo "Adding shortcut for Trash $name with value $value"
 shortcuts create "$name" "$value"
 sleep .2
 done
